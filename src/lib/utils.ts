a4b8561a2069340434877bd487f8787b9833bae8
jychoi <juyoulchoi@nate.com>
2026-02-20 20:50:12 +0900

    feat: URLSearchParams 변환 유틸 함수 추가

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// 객체를 URLSearchParams로 변환하는 유틸 함수
+export function toParams(obj: Record<string, string | undefined>) {
+  const params = new URLSearchParams();
+  Object.entries(obj).forEach(([k, v]) => {
+    if (v) params.set(k, v);
+  });
+  return params;
+}
